92fef932b4d9c7f164e20faf930fe834ee24bcca
AJ Tran <ajtrandev@gmail.com>
2020-09-21 02:03:48 -0600

    Add MapQuestService

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -1,2 +1,13 @@
 class MapQuestService
+  def self.locate(location_query)
+    conn.get '/geocoding/v1/address' do |request|
+      request.params[:location] = location_query
+    end
+  end
+
+  def self.conn
+    @conn ||= Faraday.new ENV['MAP_QUEST_URL'] do |f|
+      f.params[:key] = ENV['MAP_QUEST_KEY']
+    end
+  end
 end
