f50d1a2f3d097287660582f831fbac6cb711e0a6
AJ Tran <ajtrandev@gmail.com>
2020-09-21 11:17:50 -0600

    Add find_distance_between method to mapquest service

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -5,6 +5,17 @@ class MapQuestService
     end
   end
 
+  def self.find_distance_between(coordinates)
+    start_param = "#{coordinates[:start][:latitude]},#{coordinates[:start][:longitude]}"
+    destination_param = "#{coordinates[:destination][:latitude]},#{coordinates[:destination][:longitude]}"
+    response = conn.get '/directions/v2/route' do |request|
+      request.params[:from] = start_param
+      request.params[:to] = destination_param
+    end
+    route = JSON.parse response.body, symbolize_names: true
+    route[:route][:distance]
+  end
+
   def self.conn
     @conn ||= Faraday.new ENV['MAP_QUEST_URL'] do |f|
       f.params[:key] = ENV['MAP_QUEST_KEY']
