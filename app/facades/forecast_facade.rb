0b8c73d5f458c98428ba3d09bc8d8912187b0888
AJ Tran <ajtrandev@gmail.com>
2020-09-21 02:22:19 -0600

    refactor forecast facade

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,16 +1,23 @@
 class ForecastFacade
   def initialize params
-    @location = params[:location]
+    @location_params = params[:location]
   end
 
   def location
-    response = MapQuestService.locate(@location)
-    geocoding = JSON.parse(response.body, symbolize_names: true)
-
-    { city: geocoding[:results][0][:locations][0][:adminArea5],
+    @location ||= {
+      city: geocoding[:results][0][:locations][0][:adminArea5],
       state: geocoding[:results][0][:locations][0][:adminArea3],
       country: geocoding[:results][0][:locations][0][:adminArea1],
       latitude: geocoding[:results][0][:locations][0][:latLng][:lat],
-      longitude: geocoding[:results][0][:locations][0][:latLng][:lng] }
+      longitude: geocoding[:results][0][:locations][0][:latLng][:lng]
+    }
+  end
+
+  def current_weather
+  end
+
+  def geocoding
+    response = MapQuestService.locate @location_params
+    geocoding ||= JSON.parse response.body, symbolize_names: true
   end
 end
