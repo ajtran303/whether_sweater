1c69e05dc9c06e20e702f25f5ae88303351cb610
AJ Tran <ajtrandev@gmail.com>
2020-09-21 02:18:47 -0600

    Add current weather attributes spec

---
+++ spec/requests/forecasts/current_weather_attributes_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast response has current weather data' do
+  it 'exists' do
+    forecast_params = {
+      'location' => 'denver,co'
+    }
+
+    headers = {
+      'CONTENT_TYPE' => 'application/vnd.api+json',
+      'ACCEPT' => 'application/vnd.api+json'
+    }
+
+    get '/api/v1/forecast', headers: headers, params: forecast_params
+    forecast = parse_body(response)
+
+    expect(forecast[:data][:attributes]).to have_key :current_weather
+
+    current_weather = forecast[:data][:attributes][:current_weather]
+    expect(current_weather.keys.size).to eq 11
+
+    expect(current_weather).to have_key :date_time
+    expect(current_weather).to have_key :condition
+    expect(current_weather).to have_key :temperature
+    expect(current_weather).to have_key :high
+    expect(current_weather).to have_key :low
+    expect(current_weather).to have_key :feels_like
+    expect(current_weather).to have_key :humidity
+    expect(current_weather).to have_key :visibility
+    expect(current_weather).to have_key :uv_index
+    expect(current_weather).to have_key :sunrise
+    expect(current_weather).to have_key :sunset
+  end
+end
