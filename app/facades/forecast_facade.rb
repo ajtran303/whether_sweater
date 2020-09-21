608e05245f3c7476d50ff3899ec9414005cb90a6
AJ Tran <ajtrandev@gmail.com>
2020-09-21 03:03:33 -0600

    Add forecast current_weather

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -14,10 +14,45 @@ class ForecastFacade
   end
 
   def current_weather
+    @current_weather ||= {
+      condition: forecast[:current][:weather][0][:main],
+      temperature: forecast[:current][:temp],
+      high: forecast[:daily][0][:temp][:max],
+      low: forecast[:daily][0][:temp][:min],
+      feels_like: forecast[:current][:feels_like],
+      humidity: forecast[:current][:humidity],
+      visibility: nil,
+      uv_index: forecast[:current][:uvi],
+      sunrise: sunrise,
+      sunset: sunset
+    }
+  end
+
+  private
+
+  def sunrise
+    sunrise = EpochTimeConverter.new forecast[:current][:sunrise], forecast[:timezone_offset]
+    "#{sunrise.hour}:#{sunrise.minute} #{sunrise.meridiem}"
+  end
+
+  def sunset
+    sunset = EpochTimeConverter.new forecast[:current][:sunset], forecast[:timezone_offset]
+    "#{sunset.hour}:#{sunset.minute} #{sunset.meridiem}"
+  end
+
+  def date_time
+    EpochTimeConverter.date_time forecast[:current][:dt], forecast[:timezone_offset]
   end
 
   def geocoding
-    response = MapQuestService.locate @location_params
-    geocoding ||= JSON.parse response.body, symbolize_names: true
+    @geocoding ||= parse_body MapQuestService.locate @location_params
+  end
+
+  def forecast
+    @forecast ||= parse_body OpenWeatherService.get_forecast location[:latitude], location[:longitude]
+  end
+
+  def parse_body json
+    JSON.parse json.body, symbolize_names: true
   end
 end
