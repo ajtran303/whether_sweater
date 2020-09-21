cec9336f881d84763b63818238914012b033c040
AJ Tran <ajtrandev@gmail.com>
2020-09-21 02:12:51 -0600

    refactor forecast facade location

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -7,16 +7,10 @@ class ForecastFacade
     response = MapQuestService.locate(@location)
     geocoding = JSON.parse(response.body, symbolize_names: true)
 
-    city = geocoding[:results][0][:locations][0][:adminArea5]
-    state = geocoding[:results][0][:locations][0][:adminArea3]
-    country = geocoding[:results][0][:locations][0][:adminArea1]
-    latitude = geocoding[:results][0][:locations][0][:latLng][:lat]
-    longitude = geocoding[:results][0][:locations][0][:latLng][:lng]
-
-    { city: city,
-      state: state,
-      country: country,
-      latitude: latitude,
-      longitude: longitude }
+    { city: geocoding[:results][0][:locations][0][:adminArea5],
+      state: geocoding[:results][0][:locations][0][:adminArea3],
+      country: geocoding[:results][0][:locations][0][:adminArea1],
+      latitude: geocoding[:results][0][:locations][0][:latLng][:lat],
+      longitude: geocoding[:results][0][:locations][0][:latLng][:lng] }
   end
 end
