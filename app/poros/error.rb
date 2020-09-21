a23075c051bb8226dc9bb0768593a96e96a1672b
AJ Tran <ajtrandev@gmail.com>
2020-09-20 23:01:22 -0600

    Add unsupported error class method to error poro'

--- app/poros/error.rb
+++ app/poros/error.rb
@@ -1,5 +1,18 @@
 class Error
+  def self.unsupported(request_path)
+    params = {
+      status:'415',
+      source: request_path,
+      title: 'Unsupported Media Type',
+      detail: 'This API conforms to the JSON API Spec. ' +
+               'Requests must have the header ' +
+               '`Content-Type: application/vnd.api+json`.'
+    }
+    new(params)
+  end
+
   attr_reader :status, :source, :title, :detail
+
   def initialize(params)
     @status = params[:status]
     @source = {pointer: params[:source]}
