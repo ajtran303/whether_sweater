3ca10cdaa42fd80f23d0b10c0627bd4e04d03727
AJ Tran <ajtrandev@gmail.com>
2020-09-21 00:45:22 -0600

    add basic response shape

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -4,6 +4,26 @@ class Api::V1::ForecastController < ApplicationController
       error = Error.unsupported(request.path)
       render json: {errors: [error]}, status: 415
     end
+
+    if request_media_type_valid?
+      response = { data:
+                   { type:'forecast',
+                     id: nil,
+                     attributes: {
+                       location: nil,
+                       current_weather: nil,
+                       forecast: {
+                         eight_hour: nil,
+                         five_day: nil
+                       }
+                     }
+                   }
+                 }
+      render json: response, status: 200
+
+      # forecast = ForecastFacade.new(params[:location])
+      # render json: ForecastSerializer.to_json_api(forecast), status: 200
+    end
   end
 
   private
