23d845cea0b2d15942f36d291a199b3620c2a253
AJ Tran <ajtrandev@gmail.com>
2020-09-21 14:07:48 -0600

    remove climbing stuff

--- app/controllers/api/v1/climbing_routes_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::ClimbingRoutesController < ApplicationController
-  def index
-    climb_routes = ClimbingRoutesFacade.new(location: params[:location])
-    render json: ClimbingRoutesSerializer.to_json_api(climb_routes), status: 200
-  end
-end
