5907bff26229641357e2ef4d81014fc6f57506ce
AJ Tran <ajtrandev@gmail.com>
2020-09-21 10:41:03 -0600

    Add forecast implementation and declare next method, routes

--- app/facades/climbing_routes_facade.rb
+++ app/facades/climbing_routes_facade.rb
@@ -5,10 +5,29 @@ class ClimbingRoutesFacade
     @location = params[:location]
   end
 
+  def forecast_facade
+    @forecast_facade ||= ForecastFacade.new location: location
+  end
+
   def forecast
-    forecast = ForecastFacade.new location: location
-    summary = forecast.current_weather[:condition]
-    temperature = forecast.current_weather[:temperature]
+    summary = forecast_facade.current_weather[:condition]
+    temperature = forecast_facade.current_weather[:temperature]
     {summary: summary, temperature: temperature}
+  end
+
+  def routes
+    latitude = forecast_facade.location[:latitude]
+    longitude = forecast_facade.location[:longitude]
+    route_params = {latitude: latitude, longitude: longitude}
+    climbing_routes = MountainProjectService.find_routes(route_params)
+    climbing_routes.take(3).map do |route|
+      {
+        # name: name,
+        # type: type,
+        # rating: rating,
+        # location: location,
+        # distance_to_route: distance_to_route
+      }
+    end
   end
 end
