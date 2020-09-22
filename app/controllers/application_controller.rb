55da1b85ccd9b05de5424b3a5cd30491d93d2121
AJ Tran <ajtrandev@gmail.com>
2020-09-22 17:29:09 -0600

    improve test coverage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,7 @@
 class ApplicationController < ActionController::API
-  before_action :set_json_api_content_headers, :check_media_type
+  before_action :set_json_api_content_headers
 
   def set_json_api_content_headers
     response.headers['Content-Type'] = 'application/json'
-  end
-
-  def request_headers_valid?
-    request.media_type == 'application/json' &&
-    request.accepts[0].to_s == 'application/json'
-  end
-
-  def check_media_type
-    unless request_headers_valid?
-      error = Error.unsupported(request.path)
-      render json: {errors: [error]}, status: 415 and return
-    end
   end
 end
--- app/serializers/forecast_serializer.rb
+++
@@ -1,5 +0,0 @@
-class ForecastSerializer
-  def self.to_json_api(forecast_data)
-    forecast_data
-  end
-end
