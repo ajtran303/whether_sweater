9d143880516291de9ff2248c37554946c4dfdf68
AJ Tran <ajtrandev@gmail.com>
2020-09-22 16:58:56 -0600

    configure webmock and vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,6 +66,14 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
 
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('DONT_SHARE_MY_PROPUBLIC_SECRET_KEY') { ENV['PROPUBLICA_KEY'] }
+  config.default_cassette_options = { re_record_interval: 7.days }
+  config.configure_rspec_metadata!
+end
+
 def parse_body(response)
   JSON.parse(response.body, symbolize_names: true)
 end
