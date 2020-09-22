205cc6dbc23a9cf64feed2327e8b40fab046b33e
AJ Tran <ajtrandev@gmail.com>
2020-09-22 10:02:23 -0600

    Add specs to map quest service spec

--- spec/services/map_quest_service_spec.rb
+++ spec/services/map_quest_service_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe MapQuestService do
-  it 'can get a response'
+  it 'can get a response' do
+    a = MapQuestService.conn
+  end
+
+  it 'can locate' do
+    json = MapQuestService.locate('denver,co')
+    location = JSON.parse json.body, symbolize_names: true
+
+    expect(location).to be_a Hash
+    expect(location).to have_keys :info
+    expect(location).to have_keys :options
+    expect(location).to have_keys :results
+
+    expect(location[:info][:statuscode]).to eq 0
+  end
 end
