14e6067bafe68cf8f5b0b935e26b4f2e595670cf
AJ Tran <ajtrandev@gmail.com>
2020-09-22 10:15:39 -0600

    add another distance between example for map quest spec

--- spec/services/map_quest_service_spec.rb
+++ spec/services/map_quest_service_spec.rb
@@ -6,9 +6,10 @@ RSpec.describe MapQuestService do
     location = JSON.parse json.body, symbolize_names: true
 
     expect(location).to be_a Hash
-    expect(location).to have_keys :info
-    expect(location).to have_keys :options
-    expect(location).to have_keys :results
+    expect(location.keys.size).to eq 3
+    expect(location[:info]).to be
+    expect(location[:options]).to be
+    expect(location[:results]).to be
 
     expect(location[:info][:statuscode]).to eq 0
   end
@@ -20,4 +21,10 @@ RSpec.describe MapQuestService do
     expect(distance).to eq 61.615
   end
 
+  it 'can get distance between more coordinates' do
+    coordinates =     { :start=> {:latitude=>39.738453, :longitude=>-104.984853},
+                  :destination=> {:latitude=>40.015831,   :longitude=>-105.27927} }
+    distance = MapQuestService.find_distance_between(coordinates)
+    expect(distance).to eq 31.045
+  end
 end
