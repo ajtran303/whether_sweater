5b0f66146e8008f136d8bcca544c677e1b19f63e
AJ Tran <ajtrandev@gmail.com>
2020-09-22 09:46:07 -0600

    Change something

--- app/controllers/api/v1/climbing_routes_controller.rb
+++ app/controllers/api/v1/climbing_routes_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ClimbingRoutesController < ApplicationController
   def index
-    climb_routes = ClimbingRoutesFacade.new(location: params[:location])
+    climb_routes = ClimbingRoutesFacade.build_facade(location: params[:location])
     render json: ClimbingRoutesSerializer.to_json_api(climb_routes), status: 200
   end
 end
--- app/facades/climbing_routes_facade.rb
+++ app/facades/climbing_routes_facade.rb
@@ -1,4 +1,34 @@
 class ClimbingRoutesFacade
+  def self.build_facade(params)
+    climbing_routes = new(params)
+    {
+      data: {
+        id: nil,
+        type: 'climbing route',
+        attributes: {
+          location: climbing_routes.location,
+          forecast: {
+            summary: climbing_routes.forecast[:summary],
+            temperature: climbing_routes.forecast[:temperature]
+          },
+          routes: climbing_routes.routes.map(&climbing_route_details)
+        }
+      }
+    }
+  end
+
+  def self.climbing_route_details
+    proc do |route|
+      {
+        name: route[:name],
+        type: route[:type],
+        rating: route[:rating],
+        location: route[:location],
+        distance_to_route: route[:distance_to_route]
+      }
+    end
+  end
+
   attr_reader :location
 
   def initialize(params)
