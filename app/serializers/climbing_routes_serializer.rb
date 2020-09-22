d7b9bcf05d092154ca53b67b2e30fabc5718c2a8
AJ Tran <ajtrandev@gmail.com>
2020-09-22 10:51:30 -0600

    Change climbing routes serializer

--- app/serializers/climbing_routes_serializer.rb
+++ app/serializers/climbing_routes_serializer.rb
@@ -1,18 +1,5 @@
 class ClimbingRoutesSerializer
-  def self.to_json_api(climbing_params)
-    {
-      data: {
-        id: nil,
-        type: 'climbing route',
-        attributes: {
-          location: climbing_params[:location],
-          forecast: {
-            summary: climbing_params[:summary],
-            temperature: climbing_params[:temperature]
-          },
-          routes: climbing_params[:routes]
-        }
-      }
-    }
+  def self.to_json_api(climbing_route_data)
+    climbing_route_data
   end
 end
