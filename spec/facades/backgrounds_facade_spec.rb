44dd4bcd301a110e1a8a0666bbba746778f84786
AJ Tran <ajtrandev@gmail.com>
2020-09-22 19:47:49 -0600

    Add tests to background facade spec

--- spec/facades/backgrounds_facade_spec.rb
+++ spec/facades/backgrounds_facade_spec.rb
@@ -6,29 +6,32 @@ RSpec.describe BackgroundsFacade do
 
     top_level = [:type, :id, :attributes]
     expect(background[:data].keys).to match_array top_level
-
     expect(background[:data][:type]).to eq 'image'
     expect(background[:data][:id]).to be_nil
     expect(background[:data][:attributes]).to be_a Hash
 
     attributes = [:keyword_search, :image_url, :credit]
     expect(background[:data][:attributes].keys).to match_array attributes
-
     expect(background[:data][:attributes][:keyword_search]).to be_a String
     expect(background[:data][:attributes][:image_url]).to be_a String
     expect(background[:data][:attributes][:credit]).to be_a Hash
 
     credits = [:source, :author, :logo]
     expect(background[:data][:attributes][:credit].keys).to match_array credits
+    expect(background[:data][:attributes][:credit][:source]).to be_a String
+    expect(background[:data][:attributes][:credit][:author]).to be_a String
+    expect(background[:data][:attributes][:credit][:logo]).to be_a String
   end
 
-
   it 'instance methods' do
-    # it 'exists' do
-
     background = BackgroundsFacade.new location: 'denver,co'
     expect(background).to be_a BackgroundsFacade
     expect(background.location).to be_a String
     expect(background.weather).to be_a String
+    expect(background.keyword_search).to be_a String
+    expect(background.image_url).to be_a String
+    expect(background.source).to be_a String
+    expect(background.author).to be_a String
+    expect(background.logo).to be_a String
   end
 end
