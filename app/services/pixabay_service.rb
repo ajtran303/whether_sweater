dbb2b0e6b96789ca256a6ec332a3ff3b7a340c6d
AJ Tran <ajtrandev@gmail.com>
2020-09-22 18:33:45 -0600

    add pixabay service

--- app/services/pixabay_service.rb
+++ app/services/pixabay_service.rb
@@ -1,9 +1,19 @@
 class PixabayService < ConnectionService
   def self.find_photograph query_params
-    photo = Faraday.get '/api/' do |request|
-      request.params[:q] = query_params[:location]
-      request.params[:image_type] = 'photo'
-      request.params[:safesearch] = true
+    location_photo = parse_body search_pixabay.call query_params[:location]
+    return location_photo[:hits][0] unless location_photo[:hits].empty?
+
+    weather_photo = parse_body search_pixabay.call query_params[:weather]
+    weather_photo[:hits][0]
+  end
+
+  def self.search_pixabay
+    proc do |query_param|
+      conn.get '/api/' do |request|
+        request.params[:q] = query_param
+        request.params[:image_type] = 'photo'
+        request.params[:safesearch] = 'true'
+      end
     end
   end
 
