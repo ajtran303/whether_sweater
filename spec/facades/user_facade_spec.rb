ef1fae0b38eccaad7b71d61acfea75e6b516e906
AJ Tran <ajtrandev@gmail.com>
2020-09-22 22:25:51 -0600

    update spec

--- spec/facades/user_facade_spec.rb
+++ spec/facades/user_facade_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe UserFacade do
 
     top_level = [:type, :id, :attributes]
     expect(result[:data].keys).to match_array top_level
-    expect(result[:data][:type]).to eq 'resource'
-    expect(result[:data][:id]).to be_nil
+    expect(result[:data][:type]).to eq 'user'
+    expect(result[:data][:id]).to eq user.id
     expect(result[:data][:attributes]).to be_a Hash
 
     attributes = [:email, :api_key]
