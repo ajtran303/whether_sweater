6dabe15020afd29e39af559d44b13c5a356bf477
AJ Tran <ajtrandev@gmail.com>
2020-09-22 19:54:19 -0600

    refactor map service

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -1,23 +1,34 @@
 class MapQuestService < ConnectionService
   def self.locate(location_query)
-    location = conn.get '/geocoding/v1/address' do |request|
-      request.params[:location] = location_query
-    end
-    parse_body location
+    parse_body geocoding.call location_query
   end
 
   def self.find_distance_between(coordinates)
     start_param = "#{coordinates[:start][:latitude]},#{coordinates[:start][:longitude]}"
     destination_param = "#{coordinates[:destination][:latitude]},#{coordinates[:destination][:longitude]}"
-    travel_route = conn.get '/directions/v2/route' do |request|
-      request.params[:from] = start_param
-      request.params[:to] = destination_param
-    end
-    travel_route = parse_body travel_route
+    directions_params = { from: start_param, to: destination_param }
+    travel_route = parse_body directions.call directions_params
     travel_route[:route][:distance]
   end
 
   private
+
+  def self.directions
+    proc do |directions|
+      travel_route = conn.get '/directions/v2/route' do |request|
+        request.params[:from] = directions[:from]
+        request.params[:to] = directions[:to]
+      end
+    end
+  end
+
+  def self.geocoding
+    proc do |location|
+      conn.get '/geocoding/v1/address' do |request|
+        request.params[:location] = location
+      end
+    end
+  end
 
   def self.conn
     @conn ||= Faraday.new ENV['MAP_QUEST_URL'] do |f|
