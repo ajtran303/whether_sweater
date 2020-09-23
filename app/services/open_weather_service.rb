12b9a4a1b2fa92933433a7c3baa1b4da87f324e7
AJ Tran <ajtrandev@gmail.com>
2020-09-22 20:03:39 -0600

    update weather service

--- app/services/open_weather_service.rb
+++ app/services/open_weather_service.rb
@@ -1,15 +1,21 @@
 class OpenWeatherService < ConnectionService
   def self.get_forecast(latitude, longitude)
-    forecast = conn.get '/data/2.5/onecall' do |request|
-      request.params[:units] = 'imperial'
-      request.params[:exclude] = 'minutely'
-      request.params[:lat] = latitude
-      request.params[:lon] = longitude
-    end
-    parse_body forecast
+    coordinates = {latitude: latitude, longitude: longitude}
+    parse_body forecast.call coordinates
   end
 
   private
+
+  def self.forecast
+    proc do |coordinates|
+      conn.get '/data/2.5/onecall' do |request|
+        request.params[:units] = 'imperial'
+        request.params[:exclude] = 'minutely'
+        request.params[:lat] = coordinates[:latitude]
+        request.params[:lon] = coordinates[:longitude]
+      end
+    end
+  end
 
   def self.conn
     @conn ||= Faraday.new ENV['OPEN_WEATHER_URL'] do |f|
